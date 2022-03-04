e46aa6332d2848c765913fe28490474cb55174cc
Alaina Thompson <athompson9@hawk.iit.edu>
2022-03-04 15:19:54 -0600

    Initial Commit

---
+++ Shared/DIff_Schrodinger_Eq_App.swift
@@ -0,0 +1,17 @@
+//
+//  DIff_Schrodinger_Eq_App.swift
+//  Shared
+//
+//  Created by Alaina Thompson on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct DIff_Schrodinger_Eq_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

bdd60f18a275de25763ce9fb86c2a6b7062aaa74
Alaina Thompson <athompson9@hawk.iit.edu>
2022-03-11 09:37:11 -0600

    diff eq.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -23,7 +23,7 @@ struct ContentView: View {
     @State var potentialTypes = ["Square Well", "Linear Well", "Parabolic Well", "Square+Linear", "Square Barrier", "Triangle Barrier", "Coupled Parabolic Well", "Coupled Square Well + Field", "Harmonic Oscillator", "Kronig Penny", "Variable Kronig Penny", "KP2-a"]
     @State var selectedPotential = "Square Well"
 
-    
+    //How to make an energy picker that is dependent upon results of potentialType?
 
     
     
